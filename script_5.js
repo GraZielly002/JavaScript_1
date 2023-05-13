6a99627d15409735717c95e48bd419658ec657d0
Grazielly <104026642+GraZielly002@users.noreply.github.com>
2023-05-13 13:42:42 -0300

    Add files via upload

---
+++ script_5.js
@@ -0,0 +1,9 @@
+// Arrays é um conjunto de dados
+
+//let familia = [14,18,'Grazielly',41];
+//console.log(familia.length);
+//console.log(familia[2]);
+
+let bruna = ['Bruna Moura Bernardi',18,'Patrocínio - MG'];
+console.log(bruna);
+console.log(bruna[1]);
---
+++ script_6.js
@@ -0,0 +1,22 @@
+// Verbo + Substantivo
+//resetaCor, alterarTamanho padrão a ser seguido para nomear uma função
+//let corSite = 'azul';
+/*function resetaCor(cor){
+    corSite = cor;
+};*/
+
+//console.log(corSite);
+//resetaCor();
+//console.log(corSite);
+
+//console.log(corSite);
+//resetaCor("Vermelho");
+//console.log(corSite);
+
+let idade = 18;
+function mudarIdade(idadeNova){
+    idade = idadeNova;
+};
+//console.log(idade);
+mudarIdade(19);
+console.log(idade);
