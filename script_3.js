78b0b5451ce656cf44ead4070c4e884f469b8cbe
Grazielly <104026642+GraZielly002@users.noreply.github.com>
2023-11-22 17:44:58 -0300

    Update script_3.js

--- script_3.js
+++ script_3.js
@@ -1,6 +1,7 @@
 //Tipos Primitivos
 let nome = 'Grazielly'; //string literal(palavra).
-let idade = 18; //number literal(numero), inteiros e decimais.
+let idade = 18; //number literal(numero), inteiros(integers) e decimais(floats).
 let estaAprovado = true; //boolean(vai ajudar a construir regras no seu sistema).
 let sobrenome; //undefined(sem definição).
 let corSelecionado = null; //redefinir um valor.
+
